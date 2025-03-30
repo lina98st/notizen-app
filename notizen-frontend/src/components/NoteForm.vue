3b03ba3647f159109dd1279013added8bc464928
lina98st <alina29st@gmail.com>
2025-03-30 13:43:42 +0200

    update code

---
+++ notizen-frontend/src/components/NoteForm.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import NoteList from './components/NoteList.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Note App</h1>
+  </header>
+  <main></main>
+</template>
+
+<style scoped></style>
---
+++ notizen-frontend/src/components/NoteList.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import NoteForm from './components/NoteForm.vue'
+</script>
+
+<template></template>
+
+<style scoped></style>
